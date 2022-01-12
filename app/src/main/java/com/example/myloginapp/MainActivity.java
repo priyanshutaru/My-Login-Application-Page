2e4d1b3cfbe95638702884560e6c214e00fc3869
Priyanshu <technicalpriyanshutaru@gmail.com>
2022-01-12 09:43:58 +0530

    My Login Application Page

---
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username=(TextView) findViewById(R.id.username);
+        TextView pass= (TextView) findViewById(R.id.pass);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && pass.getText().toString().equals("admin")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
